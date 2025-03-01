3feec4072eb05d972372b29dda2f5c01c13f0b52
Ariel Mathov <amathov14@gmail.com>
2025-03-01 03:18:25 -0300

    favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,11 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>021 - From Zero to One</title>
-        <link rel="icon" href="data:," />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/favicon.svg" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0F533A" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <AuthProvider>
         <ProtectedRoute>
